247bc3816edcd52cc5f211cef39b63589a3c65be
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2017-05-03 21:05:06 -0300

    classifier finding by eucledian distance

---
+++ main.py
@@ -0,0 +1,46 @@
+import sys
+import math
+import json
+import re
+
+k_distance = 5
+target = sys.stdin.readlines()[0].split(',')
+
+file = open("data/iris.data.z1")
+swarm = file.read()
+swarm = swarm.split('\n')
+swarm = filter(lambda x: not re.match(r'^\s*$', x), swarm)
+swarm = list(map(lambda entity: entity.split(','), swarm))
+
+entity_distance_list = []
+for (i, entity) in enumerate(swarm):
+    distance = 0
+    label = entity.pop(-1)
+    for (j, attribute) in enumerate(entity):
+        distance += math.pow(float(attribute) - float(target[j]), 2)
+    distance = math.sqrt(distance)
+    pair = {}
+    pair["data"] = entity
+    pair["distance"] = distance
+    pair["label"] = label
+    entity_distance_list.append(pair)
+
+
+for i in range(0, len(entity_distance_list)):
+    for j in range(0, len(entity_distance_list)):
+        if entity_distance_list[i]['distance'] < entity_distance_list[j]['distance']:
+            tmp = entity_distance_list[j]
+            entity_distance_list[j] = entity_distance_list[i]
+            entity_distance_list[i] = tmp
+
+
+
+classes = {}
+for i in range(0, k_distance):
+    if entity_distance_list[i]["label"] in classes:
+        classes[entity_distance_list[i]["label"]] = classes[entity_distance_list[i]["label"]]+1
+    else:
+        classes[entity_distance_list[i]["label"]] = 1
+
+result = (max(classes))
+print(result)
